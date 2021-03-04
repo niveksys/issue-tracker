d9175b2e2d6b9b5af596f96886ec7dbaa93765be
Kevin Tsang <niveksys@gmail.com>
2021-03-04 23:26:29 +0800

    Initial Commit.

---
+++ src/test/java/com/niveksys/issuetracker/IssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niveksys.issuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
