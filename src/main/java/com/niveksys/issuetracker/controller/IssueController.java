77d4b4268bf965505a0b544a410e4a3700c81ab8
Kevin Tsang <niveksys@gmail.com>
2021-03-08 23:43:50 +0800

    Added list.html template for Issue.

---
+++ src/main/java/com/niveksys/issuetracker/controller/IssueController.java
@@ -0,0 +1,42 @@
+package com.niveksys.issuetracker.controller;
+
+import com.niveksys.issuetracker.repository.IssueRepository;
+import com.niveksys.issuetracker.repository.IssueTypeRepository;
+import com.niveksys.issuetracker.repository.ProjectRepository;
+import com.niveksys.issuetracker.repository.StatusRepository;
+import com.niveksys.issuetracker.repository.UserRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/issues")
+public class IssueController {
+
+    private final IssueRepository issueRepository;
+    private final IssueTypeRepository issueTypeRepository;
+    private final StatusRepository statusRepository;
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+
+    public IssueController(IssueRepository issueRepository, IssueTypeRepository issueTypeRepository,
+            StatusRepository statusRepository, ProjectRepository projectRepository, UserRepository userRepository) {
+        this.issueRepository = issueRepository;
+        this.issueTypeRepository = issueTypeRepository;
+        this.statusRepository = statusRepository;
+        this.projectRepository = projectRepository;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping({ "", "/" })
+    public String listIssues(Model model) {
+        log.debug("LIST all Issues.");
+        model.addAttribute("issues", this.issueRepository.findAll());
+        return "issues/list";
+    }
+}
